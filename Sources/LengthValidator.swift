384d367078204f97c97000db93202107869823c4
ryosuke-tani <ryosuke.tani@dena.com>
2018-05-07 11:35:52 +0900

    add ignoreWhitespaces parameter to LengthValidator

--- Sources/LengthValidator.swift
+++ Sources/LengthValidator.swift
@@ -10,11 +10,13 @@ import Foundation
 
 public struct LengthValidator: Validator {
   private let range: CountableClosedRange<Int>
+  private let ignoreWhitespaces: Bool
   private let allowBlank: Bool
   private let allowNil: Bool
   
-  public init(range: CountableClosedRange<Int>, allowBlank: Bool = false, allowNil: Bool = false) {
+  public init(range: CountableClosedRange<Int>, ignoreWhitespaces: Bool = false, allowBlank: Bool = false, allowNil: Bool = false) {
     self.range = range
+    self.ignoreWhitespaces = ignoreWhitespaces
     self.allowBlank = allowBlank
     self.allowNil = allowNil
   }
@@ -23,6 +25,9 @@ public struct LengthValidator: Validator {
     if allowNil && string == nil { return true }
     guard let string = string else { return false }
     if allowBlank && string.isEmpty { return true }
+    if ignoreWhitespaces {
+      string = string.trimmingCharacters(in: .whitespaces)
+    }
     
     return range.contains(string.characters.count)
   }
